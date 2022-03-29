10017ba025d133232620ecb26329a97f663ad836
Luca Baldini <luca.baldini@pi.infn.it>
2022-03-29 12:14:44 +0200

    More refactoring.

--- pisameet/pm2015.py
+++ pisameet/pm2015.py
@@ -24,6 +24,7 @@ import pdfrw
 
 from __init__ import logger
 from indico import retrieve_info, ConferenceInfo
+from dispatch import dispatch_posters
 
 BASE_NAME = 'pm2015'
 LOCAL_ROOT = os.path.join('/data/work/', BASE_NAME)
@@ -64,58 +65,11 @@ def dump_config_file():
     """
     CONFERENCE_INFO.dump_excel(CONFIG_FILE_PATH)
 
-def pdf_info(file_path):
-    """
-    """
-    try:
-        pdf = pdfrw.PdfReader(file_path)
-    except Exception as e:
-        logger.error('Parsing error for %s: %s', file_path, e)
-        return None, None
-    num_pages = len(pdf.pages)
-    box = pdf.pages[0].MediaBox
-    if box is None:
-        logger.error('No media box for %s...', file_path)
-        return num_pages, None
-    aspect_ratio = float(box[2]) / float(box[3])
-    return num_pages, aspect_ratio
-
-def poster_candidates(file_list):
-    """
-    """
-    candidates = []
-    for file_path in file_list:
-        if file_path.endswith('.pdf'):
-            num_pages, aspect_ratio = pdf_info(file_path)
-            if num_pages == 1 and aspect_ratio is not None and aspect_ratio < 1:
-                candidates.append(file_path)
-    return candidates
-
 def dispatch_files():
     """
     """
     ids = CONFERENCE_INFO.contribution_ids()
-    file_dict = {id_: [] for id_ in ids}
-    for file_name in os.listdir(ATTACH_FOLDER_PATH):
-        if file_name.endswith('.tstamp'):
-            continue
-        id_ = int(file_name.split('-')[0])
-        if id_ in file_dict:
-            file_dict[id_].append(os.path.join(ATTACH_FOLDER_PATH, file_name))
-    for id_, attachments in file_dict.items():
-        if len(attachments) == 0:
-            logger.error('No attachments found for contribution %d', id_)
-            continue
-        candidates = poster_candidates(attachments)
-        if len(candidates) == 1:
-            src = candidates[0]
-            file_name = os.path.basename(src)
-            dest = os.path.join(POSTER_FOLDER_PATH, file_name)
-            logger.info('Copying over %s...', file_name)
-            shutil.copyfile(src, dest)
-        else:
-            logger.warning('%d candidate posters / %d attachments for contribution %s',
-                len(candidates), len(attachments), id_)
+    dispatch_posters(ids, ATTACH_FOLDER_PATH, POSTER_FOLDER_PATH)
 
 
 
