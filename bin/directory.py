fc33b9e4e614125cedcc8a46f942a2f0d0ec44f3
Luca Baldini <luca.baldini@pi.infn.it>
2022-05-10 09:37:10 +0200

    Minor.

--- bin/directory.py
+++ bin/directory.py
@@ -22,14 +22,14 @@
 import os
 import sys
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+
 from PyQt5.QtWidgets import QApplication
 
 from pisameet import logger
 from pisameet.gui import SessionDirectory
 from pisameet.options import ArgumentParser
-
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 PARSER = ArgumentParser()
 PARSER.add_geometry()
--- bin/slideshow.py
+++ bin/slideshow.py
@@ -23,9 +23,11 @@ import argparse
 import os
 import sys
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+
 from PyQt5.QtWidgets import QApplication
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from pisameet.gui import SlideShow
 from pisameet import logger
 
