c5e59bcee9f439a836f6d16838ee50adeb8069b6
Luca Galli <luca.galli@pi.infn.it>
2024-04-10 16:52:02 +0200

    pm21 scripts adapted to read listapi.txt

--- script/rebootall.sh
+++ script/rebootall.sh
@@ -1,6 +1,30 @@
 #! /bin/bash
 
-for (( x=0; x<=24; x++ ))
+# Nome del file contenente i valori
+file="listapi.txt"
+
+# Verifica se il file esiste
+if [ ! -f "$file" ]; then
+    echo "Il file $file non esiste."
+    exit 1
+fi
+
+# Inizializza l'array per memorizzare i valori
+numero=()
+schermo=()
+
+# Leggi i valori dal file e memorizzali nell'array
+while IFS=" " read -r pinum screen; do
+    numero+=("$pinum")
+    schermo+=("$screen")
+done < "$file"
+
+# Stampa i valori letti dal file
+#echo "Valori letti dal file:"
+#echo "Prima Colonna (Numero): ${numero[*]}"
+#echo "Seconda Colonna (Schermo): ${schermo[*]}"
+
+for x in "${numero[@]}"
 do
     ip=$((100 + x))
     echo "rebooting ppm$x"
