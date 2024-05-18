f9df745e45b93a0e1733d45cc34fde9b48fed263
Luca Galli <luca.galli@pi.infn.it>
2024-05-18 18:45:42 +0200

    bug fix

--- script/autostart_browser.sh
+++ script/autostart_browser.sh
@@ -28,7 +28,7 @@ for x in "${numero[@]}"
 do
     ip=$((100 + x))
     echo "rebootin ppm$x"
-    ssh pi@192.168.30.$ip "cd pisameet; git pull; rm ../.config/autostart/pisameet*; cp pisameet_slideshow.desktop ../.config/autostart/; sudo reboot now;";
+    ssh pi@192.168.30.$ip "cd pisameet; rm ../.config/autostart/pisameet*; cp pisameet_browser.desktop ../.config/autostart/; sudo reboot now;";
 done
 
 
