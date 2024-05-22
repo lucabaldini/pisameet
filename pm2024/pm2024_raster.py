01b07b01ba95f3f8143362fbdabdd23f214c4539
Luca Baldini <luca.baldini@pi.infn.it>
2024-05-22 12:11:21 +0200

    Initial import.

--- pm2024/pm2024_raster.py
+++ pm2024/pm2024_raster.py
@@ -23,18 +23,19 @@ import argparse
 import os
 
 from pisameet import raster
+from pisameet.process import crawl
 from pm2024 import POSTER_FOLDER_PATH, POSTER_RASTER_FOLDER_PATH
 
 
 PARSER = argparse.ArgumentParser()
-PARSER.add_argument('posters', type=int, nargs='+',
+PARSER.add_argument('--posters', type=int, nargs='+',
     help='the ids of the posters to be processed')
 PARSER.add_argument('--output_folder', type=str, default=POSTER_RASTER_FOLDER_PATH,
     help='output folder for the rasterized png files')
 PARSER.add_argument('--target_width', type=int, default=1060,
     help='target width for the output png')
 PARSER.add_argument('--intermediate_width', type=int, default=4240,
-    help='intermediate minimum size')
+    help='intermediate width')
 
 
 def raster_poster(poster_id: int, target_width: int, intermediate_width: int, output_folder: str):
@@ -49,5 +50,11 @@ def raster_poster(poster_id: int, target_width: int, intermediate_width: int, ou
 
 if __name__ == '__main__':
     args = PARSER.parse_args()
-    for poster_id in args.posters:
-        raster_poster(poster_id, args.target_width, args.intermediate_width, args.output_folder)
+    if args.posters is None:
+        for file_path in crawl(POSTER_FOLDER_PATH):
+            file_name = os.path.basename(file_path)
+            poster_id = int(file_name.replace('.pdf', ''))
+            raster_poster(poster_id, args.target_width, args.intermediate_width, args.output_folder)
+    else:
+        for poster_id in args.posters:
+            raster_poster(poster_id, args.target_width, args.intermediate_width, args.output_folder)
