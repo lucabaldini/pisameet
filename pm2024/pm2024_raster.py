2ea1d0313e0d15ffae232b2cead4d17a22478dae
Luca Baldini <luca.baldini@pi.infn.it>
2024-05-23 16:58:31 +0200

    Minor.

--- pm2024/pm2024_raster.py
+++ pm2024/pm2024_raster.py
@@ -40,7 +40,7 @@ PARSER.add_argument('--autocrop', action='store_true')
 PARSER.add_argument('--overwrite', action='store_true')
 
 
-_AUTOCROP_LIST = [212, 23, 60]
+_AUTOCROP_LIST = [23, 60, 70, 212]
 
 
 
